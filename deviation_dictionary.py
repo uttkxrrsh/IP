b66c02b59677aa087c5e71cba41166a377275552
uttkxrrsh <uttkxrrsh@gmail.com>
2023-10-19 13:18:33 +0530

    deviation -ve

--- deviation_dictionary.py
+++ deviation_dictionary.py
@@ -1,1 +1,3 @@
- 
+high_dict = ["high inflation","liquidity","crude oil","high","normal monsoon","expansionary fiscal policy","capital expenditure","infrastructure", 'rising inflation']
+
+low_dict = ["economic slowdown","unemployment","controlling","manufacture","monsoon","service","economic", "increasing growth", "labour market", 'labor market']
